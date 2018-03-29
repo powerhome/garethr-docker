8775b200e857642d6e27d3ab51f876a7c9eadc3d
Pete Brown <pete.brown@powerhrg.com>
2018-03-29 16:25:00 -0400

    Use registry define from puppetlabs repo instead

--- manifests/registry.pp
+++ manifests/registry.pp
@@ -18,6 +18,9 @@
 #   Password for authentication to private Docker registry. Leave undef if
 #   auth is not required.
 #
+# [*pass_hash*]
+#   The hash to be used for receipt. If left as undef, a hash will be generated
+#
 # [*email*]
 #   Email for registration to private Docker registry. Leave undef if
 #   auth is not required.
@@ -26,35 +29,27 @@
 #   The local user to log in as. Docker will store credentials in this
 #   users home directory
 #
-# [*cwd*]
-#   The directory in which to execute docker login.
-#
 # [*receipt*]
-#   Creates a receipt file for this specific registry preventing the exec
-#   modifing config.json from triggering every puppet run.
-#   Caveat: if you modify the entry for this registry in
-#   /root/.docker/config.json outside of puppet the exec won't trigger
-#   again unless the receipt is removed.
+#   Required to be true for idempotency
 #
 define docker::registry(
-  $server      = $title,
-  $ensure      = 'present',
-  $username    = undef,
-  $password    = undef,
-  $email       = undef,
-  $local_user  = 'root',
-  $cwd         = '/root',
-  $receipt     = false,
+  Optional[String] $server                             = $title,
+  Optional[Pattern[/^present$|^absent$/]] $ensure      = 'present',
+  Optional[String] $username                           = undef,
+  Optional[String] $password                           = undef,
+  Optional[String] $pass_hash                          = undef,
+  Optional[String] $email                              = undef,
+  Optional[String] $local_user                         = 'root',
+  Optional[String] $version                            = $docker::version,
+  Optional[String] $cwd                                = '/root',
+  Optional[Boolean] $receipt                           = true,
 ) {
   include docker::params
-
-  validate_re($ensure, '^(present|absent)$')
-  validate_bool($receipt)
 
   $docker_command = $docker::params::docker_command
 
   if $ensure == 'present' {
-    if $username != undef and $password != undef and $email != undef {
+    if $username != undef and $password != undef and $email != undef and $version =~ /1[.][1-9]0?/ {
       $auth_cmd = "${docker_command} login -u '${username}' -p \"\${password}\" -e '${email}' ${server}"
       $auth_environment = "password=${password}"
     }
@@ -72,18 +67,32 @@ define docker::registry(
     $auth_environment = undef
   }
 
-  # Using Receipt?
   if $receipt {
-    file { "/root/.docker/registry-auth-puppet_receipt_${title}":
+    # no - with pw_hash
+    $local_user_strip = regsubst($local_user, '-', '', 'G')
+
+    $_pass_hash = $pass_hash ? {
+      Undef   => pw_hash("${title}${auth_environment}${auth_cmd}${local_user}", 'SHA-512', $local_user_strip),
+      default => $pass_hash
+    }
+
+    # server may be an URI, which can contain /
+    $server_strip = regsubst($server, '/', '_', 'G')
+    $_auth_command = "${auth_cmd} || rm -f \"/root/registry-auth-puppet_receipt_${server_strip}_${local_user}\""
+
+    file { "/root/registry-auth-puppet_receipt_${server_strip}_${local_user}":
       ensure  => $ensure,
-      content => pw_hash("${title}${auth_environment}${auth_cmd}${local_user}", 'SHA-512', $local_user),
+      content => $_pass_hash,
       notify  => Exec["${title} auth"],
     }
+  }
+  else {
+    $_auth_command = $auth_cmd
   }
 
   exec { "${title} auth":
     environment => $auth_environment,
-    command     => $auth_cmd,
+    command     => $_auth_command,
     user        => $local_user,
     cwd         => $cwd,
     path        => ['/bin', '/usr/bin'],
